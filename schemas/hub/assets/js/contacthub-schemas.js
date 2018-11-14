8566c69044f87d15f63bba8b4862b0281c323770
Giovanni Colangelo <giovanni.colangelo@tmt-product27-2.local>
2018-11-14 10:27:26 +0100

    js check

--- schemas/hub/assets/js/contacthub-schemas.js
+++ schemas/hub/assets/js/contacthub-schemas.js
@@ -1,119 +1,119 @@
 ;(function(document, window, showAt)
 {
-    'use strict';
-    const elSelector = '.back-to-top';
-    const element	= document.querySelector( elSelector );
-    if (!element) { return true; }
-    const wScrollShowAt = showAt || 200;
-    let wScrollCurrent = 0;
-    let wScrollBefore = 0;
-    window.addEventListener( 'scroll', function() {
-            wScrollCurrent	= window.pageYOffset;
-            if (wScrollCurrent >= wScrollShowAt) {
-                if (wScrollBefore < wScrollShowAt) {
-                    element.classList.add('show-btn');
-                }
-            } else {
-                if (wScrollBefore >= wScrollShowAt) {
-                    element.classList.remove('show-btn');
-                }
-            }
-            wScrollBefore = wScrollCurrent;
-        });
-    element.addEventListener('click', function() {
-            window.scrollTo(0,0);
-        });
+    // 'use strict';
+    // const elSelector = '.back-to-top';
+    // const element	= document.querySelector( elSelector );
+    // if (!element) { return true; }
+    // const wScrollShowAt = showAt || 200;
+    // let wScrollCurrent = 0;
+    // let wScrollBefore = 0;
+    // window.addEventListener( 'scroll', function() {
+    //         wScrollCurrent	= window.pageYOffset;
+    //         if (wScrollCurrent >= wScrollShowAt) {
+    //             if (wScrollBefore < wScrollShowAt) {
+    //                 element.classList.add('show-btn');
+    //             }
+    //         } else {
+    //             if (wScrollBefore >= wScrollShowAt) {
+    //                 element.classList.remove('show-btn');
+    //             }
+    //         }
+    //         wScrollBefore = wScrollCurrent;
+    //     });
+    // element.addEventListener('click', function() {
+    //         window.scrollTo(0,0);
+    //     });
 }( document, window, 200 ));
 
 function inviaAltezza() {
-  var bHeight = document.getElementsByTagName("body")[0].scrollHeight;
-  var sHeight = document.getElementsByClassName("AppShell__Sidebar")[0].scrollHeight;
-  var cHeight = document.getElementsByClassName("AppShell__Content")[0].scrollHeight;
-  var myHeight = Math.max(bHeight,sHeight,cHeight);  
-window.parent.postMessage(
-    {
-        event_id: 'my_cors_message',
-        data: {
-            nome: 'setHeight', 
-            altezza: myHeight + 180
-        }
-    }, 
-    "*"
-);  
+//   var bHeight = document.getElementsByTagName("body")[0].scrollHeight;
+//   var sHeight = document.getElementsByClassName("AppShell__Sidebar")[0].scrollHeight;
+//   var cHeight = document.getElementsByClassName("AppShell__Content")[0].scrollHeight;
+//   var myHeight = Math.max(bHeight,sHeight,cHeight);  
+// window.parent.postMessage(
+//     {
+//         event_id: 'my_cors_message',
+//         data: {
+//             nome: 'setHeight', 
+//             altezza: myHeight + 180
+//         }
+//     }, 
+//     "*"
+// );  
 }
 
 
 (function(funcName, baseObj) {
-    // The public function name defaults to window.docReady
-    // but you can pass in your own object and own function name and those will be used
-    // if you want to put them in a different namespace
-    funcName = funcName || "docReady";
-    baseObj = baseObj || window;
-    var readyList = [];
-    var readyFired = false;
-    var readyEventHandlersInstalled = false;
+    // // The public function name defaults to window.docReady
+    // // but you can pass in your own object and own function name and those will be used
+    // // if you want to put them in a different namespace
+    // funcName = funcName || "docReady";
+    // baseObj = baseObj || window;
+    // var readyList = [];
+    // var readyFired = false;
+    // var readyEventHandlersInstalled = false;
 
-    // call this when the document is ready
-    // this function protects itself against being called more than once
-    function ready() {
-        if (!readyFired) {
-            // this must be set to true before we start calling callbacks
-            readyFired = true;
-            for (var i = 0; i < readyList.length; i++) {
-                // if a callback here happens to add new ready handlers,
-                // the docReady() function will see that it already fired
-                // and will schedule the callback to run right after
-                // this event loop finishes so all handlers will still execute
-                // in order and no new ones will be added to the readyList
-                // while we are processing the list
-                readyList[i].fn.call(window, readyList[i].ctx);
-            }
-            // allow any closures held by these functions to free
-            readyList = [];
-        }
-    }
+    // // call this when the document is ready
+    // // this function protects itself against being called more than once
+    // function ready() {
+    //     if (!readyFired) {
+    //         // this must be set to true before we start calling callbacks
+    //         readyFired = true;
+    //         for (var i = 0; i < readyList.length; i++) {
+    //             // if a callback here happens to add new ready handlers,
+    //             // the docReady() function will see that it already fired
+    //             // and will schedule the callback to run right after
+    //             // this event loop finishes so all handlers will still execute
+    //             // in order and no new ones will be added to the readyList
+    //             // while we are processing the list
+    //             readyList[i].fn.call(window, readyList[i].ctx);
+    //         }
+    //         // allow any closures held by these functions to free
+    //         readyList = [];
+    //     }
+    // }
 
-    function readyStateChange() {
-        if ( document.readyState === "complete" ) {
-            ready();
-        }
-    }
+    // function readyStateChange() {
+    //     if ( document.readyState === "complete" ) {
+    //         ready();
+    //     }
+    // }
 
-    // This is the one public interface
-    // docReady(fn, context);
-    // the context argument is optional - if present, it will be passed
-    // as an argument to the callback
-    baseObj[funcName] = function(callback, context) {
-        if (typeof callback !== "function") {
-            throw new TypeError("callback for docReady(fn) must be a function");
-        }
-        // if ready has already fired, then just schedule the callback
-        // to fire asynchronously, but right away
-        if (readyFired) {
-            setTimeout(function() {callback(context);}, 1);
-            return;
-        } else {
-            // add the function and context to the list
-            readyList.push({fn: callback, ctx: context});
-        }
-        // if document already ready to go, schedule the ready function to run
-        if (document.readyState === "complete") {
-            setTimeout(ready, 1);
-        } else if (!readyEventHandlersInstalled) {
-            // otherwise if we don't have event handlers installed, install them
-            if (document.addEventListener) {
-                // first choice is DOMContentLoaded event
-                document.addEventListener("DOMContentLoaded", ready, false);
-                // backup is window load event
-                window.addEventListener("load", ready, false);
-            } else {
-                // must be IE
-                document.attachEvent("onreadystatechange", readyStateChange);
-                window.attachEvent("onload", ready);
-            }
-            readyEventHandlersInstalled = true;
-        }
-    }
+    // // This is the one public interface
+    // // docReady(fn, context);
+    // // the context argument is optional - if present, it will be passed
+    // // as an argument to the callback
+    // baseObj[funcName] = function(callback, context) {
+    //     if (typeof callback !== "function") {
+    //         throw new TypeError("callback for docReady(fn) must be a function");
+    //     }
+    //     // if ready has already fired, then just schedule the callback
+    //     // to fire asynchronously, but right away
+    //     if (readyFired) {
+    //         setTimeout(function() {callback(context);}, 1);
+    //         return;
+    //     } else {
+    //         // add the function and context to the list
+    //         readyList.push({fn: callback, ctx: context});
+    //     }
+    //     // if document already ready to go, schedule the ready function to run
+    //     if (document.readyState === "complete") {
+    //         setTimeout(ready, 1);
+    //     } else if (!readyEventHandlersInstalled) {
+    //         // otherwise if we don't have event handlers installed, install them
+    //         if (document.addEventListener) {
+    //             // first choice is DOMContentLoaded event
+    //             document.addEventListener("DOMContentLoaded", ready, false);
+    //             // backup is window load event
+    //             window.addEventListener("load", ready, false);
+    //         } else {
+    //             // must be IE
+    //             document.attachEvent("onreadystatechange", readyStateChange);
+    //             window.attachEvent("onload", ready);
+    //         }
+    //         readyEventHandlersInstalled = true;
+    //     }
+    // }
 })("docReady", window);
 
 docReady(inviaAltezza);
